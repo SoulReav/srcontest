81bf62502ee8a182b598787726bd119691f1112b
Alexandr Semenov <mysoulre@gmail.com>
2012-05-08 01:31:48 +0600

    dashboard update

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ class CustomIndexDashboard(Dashboard):
             modules.ModelList( title = u'Пользователи', models=('django.contrib.auth.*',)),
         )
         self.children.append(
-             modules.ModelList( title = u'Новости', models=('srcontest.news.*',))             
+             modules.ModelList( title = u'Новости', models=('srcontest.news.models.news','srcontest.news.models.categories'))
         )
 
 
