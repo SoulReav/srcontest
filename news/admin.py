db831dc575f8fa0cc0f51999cbc0c4f0e2161c46
Alexandr Semenov <mysoulre@gmail.com>
2012-05-09 19:16:26 +0600

    dashboard update

--- news/admin.py
+++ news/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from srcontest.news.models import News, Categories
 
 class NewsAdmin(admin.ModelAdmin):
-
     list_display = ('title', 'dateCreated','publish' ,'author')
     date_hierarchy = 'dateCreated'
 
